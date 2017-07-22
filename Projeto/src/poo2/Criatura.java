cc8c0862376d9632f716fd379b7bf6a1e0eb2b6b
pc <pc@pc-PC>
2017-07-22 19:16:23 -0300

    Projeto

---
+++ Projeto/src/poo2/Criatura.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho_de_poo2;
+/**
+ *
+ * @author Vinicius
+ */
+public class Criatura 
+{
+    int alimentacao;
+    int higiene;
+    int diversao;
+    int saude;
+    
+    
+}
