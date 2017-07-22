5009ff458fc00b0efbee52e53df82bdcf89efd1a
Scavoni <viniciusscavoni@gmail.com>
2017-07-22 18:13:54 -0300

    Add files via upload

---
+++ Uva.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho_de_poo2;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class Uva implements Alimento
+{
+    public int valor()
+    {
+        //Inserir o valor de UVA para o bichinho;
+        return 0;
+        
+    }
+    
+    
+}
